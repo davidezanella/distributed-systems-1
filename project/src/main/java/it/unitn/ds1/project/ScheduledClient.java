fe2e569eaef947621650496d99d1c5da99f90b7e
Francesco Lotito <lotitoqf@gmail.com>
2020-07-04 17:25:29 +0200

    add client scheduled

--- project/src/main/java/it/unitn/ds1/project/ScheduledClient.java
+++ project/src/main/java/it/unitn/ds1/project/ScheduledClient.java
@@ -10,14 +10,49 @@ import java.util.concurrent.TimeUnit;
 import java.util.Random;
 import java.io.Serializable;
 
-public class ScheduledClient extends Client {
-    private int cont_messages = 0;
+public class ScheduledClient extends AbstractActor {
+    private int cont_messages;
+    private ActorRef[] replicas;
 
     public ScheduledClient(ActorRef[] replicas) {
-        super(replicas);
+        this.replicas = replicas;
+        cont_messages = 0;
     }
 
-    protected Serializable getNewRequest() {
+    static public Props props(ActorRef[] replicas) {
+        return Props.create(ScheduledClient.class, () -> new ScheduledClient(replicas));
+    }
+
+    private int getIDRandomReplica() {
+        int idx = new Random().nextInt(this.replicas.length);
+        return idx;
+    }
+
+    @Override
+    public void preStart() {
+        
+        // Create a timer that will periodically send a message to the receiver actor
+        Cancellable timer = getContext().system().scheduler().scheduleWithFixedDelay(
+                Duration.create(5, TimeUnit.SECONDS),               // when to start generating messages
+                Duration.create(5, TimeUnit.SECONDS),               // how frequently generate them
+                getSelf(),                  // destination actor reference
+                new MsgSelf(), // the message to send
+                getContext().system().dispatcher(),                 // system dispatcher
+                getSelf()                                           // source of the message (myself)
+        );
+        
+    }
+
+    private void onMsgRWResponse(MsgRWResponse m) {
+        System.out.println("[" +
+                getSelf().path().name() +      // the name of the current actor
+                "] received a message from " +
+                getSender().path().name() +    // the name of the sender actor
+                ": value: " + m.value
+        );
+    }
+
+    private Serializable getNewRequest() {
         cont_messages = (cont_messages + 1) % 3;
         
         if(cont_messages % 3 == 1) {
@@ -29,7 +64,7 @@ public class ScheduledClient extends Client {
         }
     }
 
-    protected void onMsgSelf(MsgSelf m) {
+    private void onMsgSelf(MsgSelf m) {
         int delaySecs = 0;
         getContext().system().scheduler().scheduleOnce(
                 Duration.create(delaySecs, TimeUnit.SECONDS),
@@ -39,4 +74,13 @@ public class ScheduledClient extends Client {
                 getSelf()
         );
     }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(MsgRWResponse.class, this::onMsgRWResponse)
+                .match(MsgSelf.class, this::onMsgSelf)
+                .build();
+    }
 }
+
